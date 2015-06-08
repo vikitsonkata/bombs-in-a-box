441228ba57662c6d22e8e25d30118557636900f9
vikitsonkata <vikitsonkata@abv.bg>
2015-06-08 14:34:28 +0300

    basic idea

---
+++ game.py
@@ -0,0 +1,27 @@
+class Cell:
+    def __init__(self, color, weigth, height, deep=0):
+        pass
+    def is_fake(self):
+        pass
+    def get_position(self):
+        pass
+    
+
+class Box(Cell):
+    def __init__(self, weigth, height, deep=0):
+        highscore_moves = {}
+        highscore_pieces = {}
+        pass
+    def gravity(self):
+        pass
+    def is_clear(self):
+        pass
+    def highscore(self):
+        pass
+
+def play(Box):
+    pass
+
+def level_up():
+    # more random stuff
+    pass
